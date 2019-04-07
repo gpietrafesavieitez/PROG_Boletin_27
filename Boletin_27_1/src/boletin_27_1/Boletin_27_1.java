0fd261fbff07e7f0e0192408ef10eb61936ba3f1
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-04-07 20:28:58 +0200

    listo

---
+++ Boletin_27_1/src/boletin_27_1/Boletin_27_1.java
@@ -0,0 +1,10 @@
+package boletin_27_1;
+
+public class Boletin_27_1 {
+
+    public static void main(String[] args) {
+        //A
+        Interfaz interfaz = new Interfaz();
+        interfaz.show();
+    }
+}
